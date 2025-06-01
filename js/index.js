984c4a1633513d8276e86570293bd9c29b8ac8aa
m-irfan-km-rjk <mirfankm915@gmail.com>
2025-06-01 07:06:30 +0530

    modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 const canvas = document.querySelector("#world");
 const ctx = canvas.getContext('2d');
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const colours = ["Red","Black","Green","Yellow"];
+
+var Animal = ( id,color ) => {
+    this.age = 0;
+    this.size = 1;
+    this.id = id;
+    this.color = colours[color];
+
+    this.update = () => {
+
+    };
+};
